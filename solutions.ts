3941283f2f77b2b78182a2ac15dab23608efc8a2
Shadow-Blaxe-123 <shamunahmed38@gmail.com>
2025-05-08 11:53:15 +0600

    Done with problem no.1

--- solutions.ts
+++ solutions.ts
@@ -1,15 +1,11 @@
-/*
-Problem 1:
-Description: Create a function that takes a string and an optional boolean.
+function formatString(input: string, toUpper?: boolean) : string{
+    if(toUpper === true || toUpper === undefined){
+        return input.toUpperCase()
+    } else{
+        return input.toLowerCase();
+    }
+}
 
-If the boolean is true or not provided, return the string in uppercase.
-If the boolean is false, return the string in lowercase.
-Function Signature:
-
-function formatString(input: string, toUpper?: boolean): string
-Example:
-
-formatString("Hello");          // Output: "HELLO"
-formatString("Hello", true);   // Output: "HELLO"
-formatString("Hello", false);  // Output: "hello"
-*/
+// console.log(formatString("Hello"));          // Output: "HELLO"
+// console.log(formatString("Hello", true));   // Output: "HELLO"
+// console.log(formatString("Hello", false));  // Output: "hello"
