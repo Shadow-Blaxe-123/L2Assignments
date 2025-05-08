d1754c8e31737e9e131e0f91722f5896f225783c
Shadow-Blaxe-123 <shamunahmed38@gmail.com>
2025-05-08 13:11:30 +0600

    Improved no.5

--- solutions.ts
+++ solutions.ts
@@ -57,5 +57,5 @@ class Car extends Vehicle {
 function processValue(value: string | number): number{
     return typeof value  === 'string' ? value.length : value * 2
 }
-console.log(processValue("hello")); // Output: 5
-console.log(processValue(10));      // Output: 20
+// console.log(processValue("hello")); // Output: 5
+// console.log(processValue(10));      // Output: 20
