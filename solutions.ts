fe38a26f6f7e9b0a27eab7b1e16a578f0e274221
Shadow-Blaxe-123 <shamunahmed38@gmail.com>
2025-05-08 15:33:58 +0600

    All problems solved

--- solutions.ts
+++ solutions.ts
@@ -6,31 +6,15 @@ function formatString(input: string, toUpper?: boolean) : string{
     }
 }
 
-// console.log(formatString("Hello"));          // Output: "HELLO"
-// console.log(formatString("Hello", true));   // Output: "HELLO"
-// console.log(formatString("Hello", false));  // Output: "hello"
 
 function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[] {
     return items.filter((item) => item.rating > 4)
 }
 
-// const books = [
-//     { title: "Book A", rating: 4.5 },
-//     { title: "Book B", rating: 3.2 },
-//     { title: "Book C", rating: 5.0 }
-//   ];
-  
-//   console.log(filterByRating(books) )
-//   // Output: [ { title: "Book A", rating: 4.5 }, { title: "Book C", rating: 5.0 } ]
-
-
 function concatenateArrays<T>(...arrays: T[][]): T[]{ 
     let arr : T[] = [];
     return (arr.concat(...arrays));
 }
-
-// console.log(concatenateArrays(["a", "b"], ["c"]));       // Output: ["a", "b", "c"]
-// console.log(concatenateArrays([1, 2], [3, 4], [5]));     // Output: [1, 2, 3, 4, 5]
 
 
 class Vehicle {
@@ -49,16 +33,11 @@ class Car extends Vehicle {
     }
 }
 
-// const myCar = new Car("Toyota", 2020, "Corolla");
-// console.log(myCar.getInfo());   // Output: "Make: Toyota, Year: 2020"
-// console.log(myCar.getModel());  // Output: "Model: Corolla"
-
 
 function processValue(value: string | number): number{
     return typeof value  === 'string' ? value.length : value * 2
 }
-// console.log(processValue("hello")); // Output: 5
-// console.log(processValue(10));      // Output: 20
+
 
 
 interface Product {
@@ -79,14 +58,6 @@ interface Product {
     }
 }
 
-// const products = [
-//   { name: "Pen", price: 10 },
-//   { name: "Notebook", price: 25 },
-//   { name: "Bag", price: 50 }
-// ];
-
-// console.log(getMostExpensiveProduct(products));  
-// Output: { name: "Bag", price: 50 }
 
 
 enum Day {
@@ -106,6 +77,21 @@ enum Day {
         return "Weekday";
     }
   }
-  
-//   console.log(getDayType(Day.Monday));   // Output: "Weekday"
-//   console.log(getDayType(Day.Sunday));   // Output: "Weekend"
+
+
+const createPromise = (n: number) : Promise<number> =>{
+    return new Promise((resolve, reject) =>{
+        if (n > 0) {
+            setTimeout(() =>{
+                resolve( n * n)
+            }, 1000);
+        } else {
+            reject("Error: Negative number not allowed")
+        }
+    })
+}
+
+async function squareAsync(n: number): Promise<number> {
+    // const squaredNumber: number = await createPromise(n)
+    return await createPromise(n);
+}
